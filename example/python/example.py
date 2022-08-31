d450edf62943d1990d18de81885b74dde0fec53f
kbrezhnyev <kyrylo.brezhnyev@altran.com>
2022-08-31 18:12:37 +0200

    cleaned up code

--- example/python/example.py
+++ example/python/example.py
@@ -2,23 +2,22 @@ import ctypes
 import os
 import sys
 
-from certifi import contents
-
 def process(xodrpath):
   # Load the shared library into ctypes
   libname = os.getcwd() + "/../../lib/libXodrBuilder.so"
   xodrLib = ctypes.cdll.LoadLibrary(libname)
+  # get xodr builder
   getXodrFun = xodrLib.getXodrBuilder
   getXodrFun.argtypes = [ctypes.POINTER(ctypes.c_char), ctypes.c_double]
   getXodrFun.restype = ctypes.POINTER(ctypes.c_double)
   xb = getXodrFun(xodrpath[0].encode('utf-8'), 1.0)
-  
+  # get number of points
   getNofPfun = xodrLib.getNumberOfPoints
   getNofPfun.argtypes = [ctypes.c_void_p]
   getNofPfun.restype = ctypes.c_longlong
   pointsN = getNofPfun(xb)
   print(pointsN)
-
+  # get points
   getPointsFun = xodrLib.getAllPoints
   getPointsFun.argtypes = [ctypes.c_void_p]
   pn = pointsN*4
